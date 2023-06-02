7b9ce4b2c678b2e97c09289d027fdd69c1a80908
Mahdi <hassanim430@gmail.com>
2023-06-02 09:13:12 +0430

    Create model schema

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const routes = require("./src/routes/crm");
 const app = express();
 const PORT = 3000;
+
+// Mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/CRM", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Body parser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 routes(app);
 
