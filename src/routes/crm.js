7f35fb1f6a33417e6825ffef60c1e961c34c9aa9
Mahdi <hassanim430@gmail.com>
2023-06-02 08:52:48 +0430

    Testing middlewares in apis

--- src/routes/crm.js
+++ src/routes/crm.js
@@ -1,23 +1,30 @@
 const routes = (app) => {
+  app
+    .route("/contact")
 
-  app.route("/contact")
-  
-  .get((req, res) => {
-    res.send("Get request received successfuly");
-  })
+    .get(
+      (req, res, next) => {
+        //middleware
+        console.log(`Request from ${req.originalUrl}`);
+        console.log(`Request type ${req.method}`);
+        next();
+      },
+      (req, res, next) => {
+        res.send("Get request received successfuly");
+      }
+    )
 
-  .post((req, res) => {
-    res.send("Post request received successfuly");
-  })
+    .post((req, res) => {
+      res.send("Post request received successfuly");
+    })
 
-  .put((req, res) => {
-    res.send("Put request received successfuly");
-  })
+    .put((req, res) => {
+      res.send("Put request received successfuly");
+    })
 
-  .delete((req, res) => {
-    res.send("Delete request received successfuly");
-  })
-
+    .delete((req, res) => {
+      res.send("Delete request received successfuly");
+    });
 };
 
 module.exports = routes;
